d304229cbcfacb106d677bd80fde2b40efd519cf
Mohit Kumar <mohit.sircl@gmail.com>
2023-07-25 12:08:29 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// Zy3xT07x0qJH2sRk  username-mohit6644
+
+console.log("hello world!")
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mohit6644:Zy3xT07x0qJH2sRk@employee.wqvtxkl.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
